b0bfb45bdbee0150c42ecfe4469ca0c1f192baaa
SAHA-YJB <pkomom123@naver.com>
2023-06-23 22:04:22 +0900

    반복컴포넌트출력 연습 1 배열

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "App.css";
 
 function App() {
+  const arr = ["감자", "고구마", "오이", "가지", "옥수수"];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="inner">
+      {arr.filter((item) => {
+        return item !== "오이";
+      })
+      .map((item) =>{
+        return <div className="component-style">{item}</div>;
+      })
+      }
     </div>
   );
 }
