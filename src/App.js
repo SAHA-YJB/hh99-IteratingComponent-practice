db21cf6db58324477fe5f71ef13f08d8e138aa9e
SAHA-YJB <pkomom123@naver.com>
2023-06-23 22:58:05 +0900

    create

--- src/App.js
+++ src/App.js
@@ -1,27 +1,46 @@
 import React from "react";
 import "App.css";
+import { useState } from "react";
 
 function App() {
-  const users = [
+  const [users, setUsers] = useState([
     { id: 1, age: 34, name: "zxc" },
     { id: 2, age: 25, name: "asd" },
     { id: 3, age: 12, name: "qwe" },
     { id: 4, age: 65, name: "pio" },
-  ];
+  ]);
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+
+  const inNameValue = (event) => {
+    setName(event.target.value);
+  };
+  const inAgeValue = (event) => {
+    setAge(event.target.value);
+  };
+  const clickAddBtnHandler = () => {
+    const newUser = { id: users.length +1, age, name}
+    setUsers([...users, newUser])
+    setName('');
+    setAge('');
+  }
+
   return (
-    <div className="inner">
-      {
-      users.map((user) => {
-        return (
-          <div key={user.id} className="component-style">
-            {user.name} - {user.age}
-          </div>
-        );
-      })
-      }
-      {/* <div className="component-style">
-        {users[0].name} - {users[0].age}
-      </div> */}
+    <div>
+      <div>
+        이름: <input value={name} onChange={inNameValue} />
+        나이: <input value={age} onChange={inAgeValue} />
+      </div>
+        <button onClick={clickAddBtnHandler}>추가</button>
+      <div className="inner">
+        {users.map((user) => {
+          return (
+            <div key={user.id} className="component-style">
+              {user.name} - {user.age}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
