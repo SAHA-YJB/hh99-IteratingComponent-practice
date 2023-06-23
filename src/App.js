39a169bbb96c6de067278367152cfa74989edc04
SAHA-YJB <pkomom123@naver.com>
2023-06-23 22:24:23 +0900

    배열안 객체 반복해서 출력하기

--- src/App.js
+++ src/App.js
@@ -2,16 +2,26 @@ import React from "react";
 import "App.css";
 
 function App() {
-  const arr = ["감자", "고구마", "오이", "가지", "옥수수"];
+  const users = [
+    { id: 1, age: 34, name: "zxc" },
+    { id: 2, age: 25, name: "asd" },
+    { id: 3, age: 12, name: "qwe" },
+    { id: 4, age: 65, name: "pio" },
+  ];
   return (
     <div className="inner">
-      {arr.filter((item) => {
-        return item !== "오이";
-      })
-      .map((item) =>{
-        return <div className="component-style">{item}</div>;
+      {
+      users.map((user) => {
+        return (
+          <div key={user.id} className="component-style">
+            {user.name} - {user.age}
+          </div>
+        );
       })
       }
+      {/* <div className="component-style">
+        {users[0].name} - {users[0].age}
+      </div> */}
     </div>
   );
 }
